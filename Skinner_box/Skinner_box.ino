1fb88a8f080407f8ea94826d36075140439d7b4a
zakopuro <zakopuro@Zakopouros-PC.local>
2018-12-17 21:36:06 +0900

    motor動作

---
+++ Skinner_box/Skinner_box.ino
@@ -0,0 +1,16 @@
+#include <Servo.h> 
+
+Servo servo;
+
+void setup(){
+  servo.attach(8);
+  servo.write(0);
+  delay(2000);
+}
+
+void loop(){
+  servo.write(90);
+  delay(1000);
+  servo.write(0);
+  delay(1000);
+}
