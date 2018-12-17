3ab613d36f2db53a86e2a62147ca73f175c741b1
zakopuro <zakopuro@gmail.com>
2018-12-17 23:01:09 +0900

    motor & sensor

--- Skinner_box/Skinner_box.ino
+++ Skinner_box/Skinner_box.ino
@@ -1,16 +1,34 @@
 #include <Servo.h> 
 
 Servo servo;
+#define HUMAN_SENSOR_PIN  (2)
+#define SERVO_MOTOR_PIN   (8)
+#define LED_PIN           (13)
+// static int  motorstate = 0;
 
 void setup(){
-  servo.attach(8);
+  servo.attach(SERVO_MOTOR_PIN);
   servo.write(0);
+  pinMode(LED_PIN,OUTPUT);
+  pinMode(HUMAN_SENSOR_PIN,INPUT);
   delay(2000);
 }
 
 void loop(){
-  servo.write(90);
-  delay(2000);
-  servo.write(0);
-  delay(3000);
+  static int a;
+
+  if (digitalRead(HUMAN_SENSOR_PIN) == LOW){
+    // Do Nothing
+  } else{
+    if ( a == 0){
+      servo.write(90);
+      a = 1;
+      digitalWrite(LED_PIN,HIGH);
+    } else{
+      servo.write(0);
+      a = 0;
+      digitalWrite(LED_PIN,LOW);
+    }
+    delay(3000);
+  }
 }
